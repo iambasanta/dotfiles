5be34bbc56217aa5fa9abbb498aa02af8ec8048e
iambasanta <sangpangbasanta@gmail.com>
2022-07-26 17:16:35 +0545

    feat: Add cheat sheet script

---
+++ cheat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+languages=$(echo "bash c cpp css golang html javascript lua nodejs php python rust solidity tmux typescript zsh" | tr " " "\n")
+core_utils=$(echo "awk cargo cat chmod chown cp docker docker-compose find git git-commit git-rebase git-status git-worktree grep head jq kill less ls lsof make man mv ps rename rg rm sed ssh stow tail tar tldr tr xargs" | tr " " "\n")
+selected=$(echo -e "$languages\n$core_utils" | fzf)
+
+read -p "Enter Query: " query
+
+if echo "$languages" | grep -qs $selected; then
+    curl cht.sh/$selected/$(echo "$query" | tr " " "+")
+else
+    curl cht.sh/$selected~$query
+fi
