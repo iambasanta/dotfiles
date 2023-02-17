2006db3c95d7f107aa1e2dc696ae1592a108afb7
iambasanta <sangpangbasanta@gmail.com>
2023-02-17 17:49:42 +0545

    Remap mark navigation keymaps

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -11,14 +11,14 @@ end
 vim.keymap.set("n", "<Leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-h>", function()
+vim.keymap.set("n", "<C-j>", function()
 	ui.nav_file(1)
 end)
 
-vim.keymap.set("n", "<C-t>", function()
+vim.keymap.set("n", "<C-k>", function()
 	ui.nav_file(2)
 end)
 
-vim.keymap.set("n", "<C-n>", function()
+vim.keymap.set("n", "<C-l>", function()
 	ui.nav_file(3)
 end)
